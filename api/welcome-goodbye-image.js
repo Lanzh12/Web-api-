a611535220f88885f4a15c5b461ad5f82ca93d8a
LemonSync <grouplemon0@gmail.com>
2025-06-05 11:45:44 +0700

    Update welcome-goodbye-image.js

--- api/welcome-goodbye-image.js
+++ api/welcome-goodbye-image.js
@@ -5,14 +5,15 @@ const router = express.Router();
 router.get('/', async (req, res) => {
   const { name, profile, welcome } = req.query;
 
-  if (!name || !profile) {
+  if (!name) {
     return res.status(400).json({ error: 'Parameter name dan profile wajib diisi.' });
   }
 
   try {
+    const finalProfile = profile || "";
     const response = await axios.post('https://lemon-welcome.vercel.app/api/generate-image', {
       name,
-      image: profile,
+      image: finalProfile,
       welcome: welcome === 'true'
     }, {
       headers: {
